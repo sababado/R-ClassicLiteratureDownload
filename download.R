ecc6631063238d2dfa11d4c1d52baf85945977a4
Rolo <robert.szabo@coxautoinc.com>
2022-02-24 22:32:38 -0600

    Filtering books

--- download.R
+++ download.R
@@ -4,3 +4,29 @@ library(stringr)
 library(ggplot2)
 
 books <- read_csv('data/books.csv')
+
+twain <- dplyr::filter(books, author == "Twain, Mark")
+
+twain_refined <- twain %>%
+  select(title, author, downloads, avg_words_per_sentence, sentences)
+
+twain_by_download <- arrange(twain_refined, desc(downloads))
+
+unique_books <- function(data, column='title') {
+  items <- pull(data, column)
+  duplicates <- list()
+  
+  for (item in items) {
+    match <- agrep(item, items)
+    last <- match[-1]
+    if(length(last) > 0) {
+      duplicates[[last]] <- last
+    }
+  }
+  print(duplicates)
+  remove <- unique(unlist(duplicates))
+  data[-remove,]
+}
+
+twain_unique <- unique_books(twain_by_download)
+
